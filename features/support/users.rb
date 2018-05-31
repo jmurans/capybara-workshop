ed04cee204ac2682141d5b5c0426b8ea6b07b0c3
jmurans <Drakonich@gmail.com>
2018-05-31 16:39:56 +0300

    Added placeholder for valid user

--- features/support/users.rb
+++ features/support/users.rb
@@ -4,11 +4,16 @@ module Users
         @user_with_invalid_credentials ||= User.new(args = {})
         @user_with_invalid_credentials
     end
+
+    def Users.valid_user
+        @valid_user ||= User.valid_user
+        @valid_user
+    end
     
     class User
         attr_reader :email, :password, :incPassword, :projectName
         def initialize(args = {})
-            defaults = {email: DateTime.now.strftime('%Q')+'@test.com', password: DateTime.now.strftime('%Q'), incPassword: DateTime.now.strftime('%Q')+'1' , projectName: DateTime.now}
+            defaults = {email: DateTime.now.strftime('%Q')+'@test.com', password: DateTime.now.strftime('%Q'), incPassword: DateTime.now.strftime('%Q') , projectName: DateTime.now}
             args = defaults.merge(args)
             @email = args[:email]
             @password = args[:password]
